a4230287189a847d18eca4b41e6f4c6c0148f079
Arturo <alopez96@ucsc.edu>
2018-10-14 10:02:52 -0700

    analytics included in html page

--- server2.js
+++ server2.js
@@ -20,13 +20,3 @@ var lineDrawing = anime({
   direction: 'alternate',
   loop: true
 });
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127434524-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-127434524-1');
-</script>
