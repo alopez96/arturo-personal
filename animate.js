195611fca465f2f9adcf97d7c141f91a3f92e8ca
Arturo <alopez96@ucsc.edu>
2018-11-02 11:26:43 -0700

    projects ui update

--- animate.js
+++ animate.js
@@ -1,1 +1,60 @@
 $('#home-btn').addClass('animated fadeInUp');
+
+
+$(document).ready(function(){
+    $(document.getElementById("tech-btn")).click(function(){
+        $("#list").fadeIn();
+        $("#list").fadeIn("slow");
+        $("#list").fadeIn(3000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(document.getElementById("tech-btn2")).click(function(){
+        $("#list2").fadeIn();
+        $("#list2").fadeIn("slow");
+        $("#list2").fadeIn(3000);
+    });
+});
+
+$(document).ready(function(){
+    $(document.getElementById("tech-btn3")).click(function(){
+        $("#list3").fadeIn();
+        $("#list3").fadeIn("slow");
+        $("#list3").fadeIn(3000);
+    });
+});
+
+$(document).ready(function(){
+    $(document.getElementById("tech-btn4")).click(function(){
+        $("#lis4").fadeIn();
+        $("#list4").fadeIn("slow");
+        $("#list4").fadeIn(3000);
+    });
+});
+
+$(document).ready(function(){
+    $(document.getElementById("tech-btn5")).click(function(){
+        $("#lis5").fadeIn();
+        $("#list5").fadeIn("slow");
+        $("#list5").fadeIn(3000);
+    });
+});
+
+$(document).ready(function(){
+    $(document.getElementById("tech-btn6")).click(function(){
+        $("#lis6").fadeIn();
+        $("#list6").fadeIn("slow");
+        $("#list6").fadeIn(300000);
+    });
+});
+
+
+$(document).ready(function(){
+    $(document.getElementById("tech-btn7")).click(function(){
+        $("#lis7").fadeIn();
+        $("#list7").fadeIn("slow");
+        $("#list7").fadeIn(3000000000);
+    });
+});
