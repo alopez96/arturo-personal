a0fde530f3b57c8d0e15df4509b5a227cc54b07f
Arturo <alopez96@ucsc.edu>
2019-10-15 21:26:11 -0700

    add event app, refactored tech used button js action

--- animate.js
+++ animate.js
@@ -1,89 +1,10 @@
 $('#home-btn').addClass('animated fadeInRight');
 
 
-$(document).ready(function(){
-    $(document.getElementById("tech-btn")).click(function(){
-        $("#list").fadeIn();
-        $("#list").fadeIn("slow");
-        $("#list").fadeIn(3000);
+const dropDown = function (list) {
+    $(document).ready(function(){
+        $('#'+list).fadeIn();
+        $('#'+list).fadeIn("slow");
+        $('#'+list).fadeIn(3000);
     });
-});
-
-
-$(document).ready(function(){
-    $(document.getElementById("tech-btn2")).click(function(){
-        $("#list2").fadeIn();
-        $("#list2").fadeIn("slow");
-        $("#list2").fadeIn(3000);
-    });
-});
-
-$(document).ready(function(){
-    $(document.getElementById("tech-btn3")).click(function(){
-        $("#list3").fadeIn();
-        $("#list3").fadeIn("slow");
-        $("#list3").fadeIn(3000);
-    });
-});
-
-$(document).ready(function(){
-    $(document.getElementById("tech-btn4")).click(function(){
-        $("#lis4").fadeIn();
-        $("#list4").fadeIn("slow");
-        $("#list4").fadeIn(3000);
-    });
-});
-
-$(document).ready(function(){
-    $(document.getElementById("tech-btn5")).click(function(){
-        $("#lis5").fadeIn();
-        $("#list5").fadeIn("slow");
-        $("#list5").fadeIn(3000);
-    });
-});
-
-$(document).ready(function(){
-    $(document.getElementById("tech-btn6")).click(function(){
-        $("#lis6").fadeIn();
-        $("#list6").fadeIn("slow");
-        $("#list6").fadeIn(300000);
-    });
-});
-
-
-$(document).ready(function(){
-    $(document.getElementById("tech-btn7")).click(function(){
-        $("#lis7").fadeIn();
-        $("#list7").fadeIn("slow");
-        $("#list7").fadeIn(3000000000);
-    });
-});
-
-
-$(document).ready(function(){
-    $(document.getElementById("tech-btn8")).click(function(){
-        $("#lis8").fadeIn();
-        $("#list8").fadeIn("slow");
-        $("#list8").fadeIn(3000000000);
-    });
-    $(document.getElementById("tech-btn9")).click(function(){
-        $("#lis9").fadeIn();
-        $("#list9").fadeIn("slow");
-        $("#list9").fadeIn(3000000000);
-    });
-    $(document.getElementById("tech-btn10")).click(function(){
-        $("#lis10").fadeIn();
-        $("#list10").fadeIn("slow");
-        $("#list10").fadeIn(3000000000);
-    });
-    $(document.getElementById("tech-btn11")).click(function(){
-        $("#lis11").fadeIn();
-        $("#list11").fadeIn("slow");
-        $("#list11").fadeIn(3000000000);
-    });
-    $(document.getElementById("tech-btn12")).click(function(){
-        $("#lis12").fadeIn();
-        $("#list12").fadeIn("slow");
-        $("#list12").fadeIn(3000000000);
-    });
-});
+}
