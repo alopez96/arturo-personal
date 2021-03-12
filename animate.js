cea220802d24f2b8aa183bbc92b40bb6ed29002e
Arturo Lopez <arturolopez.developer@gmail.com>
2021-03-11 17:03:02 -0800

    cleaned up code and added js for circle hover, but not implementing

--- animate.js
+++ animate.js
@@ -1,10 +1,34 @@
-$('#home-btn').addClass('animated fadeInRight');
+const $bigBall = document.querySelector('.cursor__ball--big');
+const $smallBall = document.querySelector('.cursor__ball--small');
+const $hoverables = document.querySelectorAll('.hoverable');
 
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
 
-const dropDown = function (list) {
-    $(document).ready(function(){
-        $('#'+list).fadeIn();
-        $('#'+list).fadeIn("slow");
-        $('#'+list).fadeIn(3000);
-    });
+// Move the cursor
+function onMouseMove(e) {
+  gsap.to($bigBall, .4, {
+    x: e.pageX - 15,
+    y: e.pageY - 15
+  })
+  gsap.to($smallBall, .1, {
+    x: e.pageX - 5,
+    y: e.pageY - 7
+  })
+}
+
+// Hover an element
+function onMouseHover() {
+  gsap.to($bigBall, .3, {
+    scale: 4
+  })
+}
+function onMouseHoverOut() {
+  gsap.to($bigBall, .3, {
+    scale: 1
+  })
 }
